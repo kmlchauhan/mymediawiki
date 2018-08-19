8f4f6b1e1cf48c24bf254ba07a9ce8142d8bf76f
Kamal Chauhan <kmlchauhan@gmail.com>
2018-08-19 13:59:40 +0530

    Updated the install commands.

--- install-awscli.sh
+++ install-awscli.sh
@@ -1,5 +1,3 @@
 sudo apt-get -y update
-sudo apt-get install -y python2-pip
-sudo apt-get install -y python3-pip
-sudo pip install -y awscli
-sudo pip3 install -y awscli
+sudo apt-get install -y python-pip
+sudo pip install awscli
