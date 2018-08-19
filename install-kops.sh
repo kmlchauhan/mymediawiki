f4d329582f3c748eabc150fd5d02cee0b9548612
Kamal Chauhan <kmlchauhan@gmail.com>
2018-08-19 14:22:48 +0530

    Updated the install script.

--- install-kops.sh
+++ install-kops.sh
@@ -1,10 +1,15 @@
+KOPS_AVAILABLE=`which kops`
+KOPS_LATEST=`curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4`
+
 function install_kops {
-    if [ -z $(which kops) ]
-       then
-           curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
-           chmod +x kops-linux-amd64
-           mv kops-linux-amd64 /usr/local/bin/kops
-       else
-           echo "kops is most likely installed"
-       fi
+  if [ -z ${KOPS_AVAILABLE} ]; then
+     curl -LO https://github.com/kubernetes/kops/releases/download/${KOPS_LATEST}/kops-linux-amd64
+     chmod +x kops-linux-amd64
+     sudo mv kops-linux-amd64 /usr/local/bin/kops
+     kops version
+  else
+     echo "kops is most likely installed"
+  fi
 }
+
+install_kops
