ab298390e8e8ea3c71c76ff24d7fc6bdd31bdbcb
Kamal Chauhan <kmlchauhan@gmail.com>
2018-08-19 14:49:01 +0530

    Updated  the terraform install script.

--- install-terraform.sh
+++ install-terraform.sh
@@ -1,15 +1,19 @@
-TERRAFORM_ZIP_FILE=terraform_0.11.7_linux_amd64.zip
-TERRAFORM=https://releases.hashicorp.com/terraform/0.11.7
+TERRAFORM_ZIP_FILE=terraform_0.11.8_linux_amd64.zip
+TERRAFORM=https://releases.hashicorp.com/terraform/0.11.8
 TERRAFORM_BIN=terraform
- 
+
 function install_terraform {
-    if [ -z $(which $TERRAFORM_BIN) ]
-       then
-           wget ${TERRAFORM}/${TERRAFORM_ZIP_FILE}
-           unzip ${TERRAFORM_ZIP_FILE}
-           sudo mv ${TERRAFORM_BIN} /usr/local/bin/${TERRAFORM_BIN}
-           rm -rf ${TERRAFORM_ZIP_FILE}
+    if [ -z $(which $TERRAFORM_BIN) ]; then
+       wget ${TERRAFORM}/${TERRAFORM_ZIP_FILE}
+       unzip ${TERRAFORM_ZIP_FILE}
+       sudo mv ${TERRAFORM_BIN} /usr/local/bin/${TERRAFORM_BIN}
+       rm -rf ${TERRAFORM_ZIP_FILE}
+       terraform version
     else
        echo "Terraform is most likely installed"
     fi
 }
+
+sudo apt-get install unzip -y
+
+install_terraform
