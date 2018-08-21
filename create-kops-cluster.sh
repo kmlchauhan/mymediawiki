0a4ac0e85613eed2879fb7021c1599fc0c50092b
Kamal Chauhan <kmlchauhan@gmail.com>
2018-08-21 17:02:47 +0530

    Updated the script with proper comments.

--- create-kops-cluster.sh
+++ create-kops-cluster.sh
@@ -5,16 +5,30 @@
 # *******************************
 
 kops create cluster \
-  --name=krakcconsultants.com \
-  --state=s3://kops-state-k070918 \
+# This is the name of the cluster
+  --name=clustername.com \
+# This is S3 bucket on AWS (Object Storage)
+  --state=s3://terraform-kops-s3-storage \
+# Role Based Authorization
   --authorization RBAC \
+# Actual Data Center in N. Virginia
   --zones=ap-south-1a \
-  --image=${AMI_ID} \
+# How many nodes we want
   --node-count=2 \
+# How much powerful EC2 instance in the cluster we want
   --node-size=t2.micro \
   --master-size=t2.micro \
+# How many masters in K8s cluster we want
   --master-count=1 \
-  --dns-zone=krakcconsultants.com \
-  --out=krakcconsultants_terraform \
+# Hosted zone - we need to create it in advance in AWS route53
+# you can define your own DNS name
+# make sure that you have your own domain name
+  --dns-zone=zonename.com \
+# This is the name of the output folder where 
+# kops will generate ==> Terraform code
+  --out=clustername_terraform \
+# Target is terraform code
   --target=terraform \
-  --ssh-public-key=~/kubernetes/kmlchauhan-terraform.pub
+# Create key pair files using this command
+# sshkeygen -f ~/.ssh/terraform-key
+  --ssh-public-key=~/.ssh/kmlchauhan_terraform.pub
