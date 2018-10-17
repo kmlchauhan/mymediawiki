184623741a8cf5b6abc865e3d0950606db14b303
Kamal Chauhan <kmlchauhan@gmail.com>
2018-10-17 15:27:48 +0530

    Updated URL for K8 Dashboard

--- pull-kubeconfig.sh
+++ pull-kubeconfig.sh
@@ -7,5 +7,5 @@ echo "==========================================================================
 cat ~/.kube/config | grep -i username | head -n 1
 cat ~/.kube/config | grep -i password | head -n 1
 echo " url: https://api.${KOPS_NAME}/api/v1/proxy/namespaces/kube-system/services/kubernetes-dashboard/#/persistentvolume?namespace=_all"
-echo " install dashboard: kubectl create -f https://rawgit.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard.yaml"
+echo " install dashboard: kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml"
 echo "=================================================================================================================================="
